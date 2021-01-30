b18dc410d08da91f1062d12bf5a5e7d52f7b3a1c
Gülistan Öztürk <gulistanozturk137@gmail.com>
2021-01-30 22:59:48 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionary<tKey, tValue>
+    {
+        tKey[] keys;
+        tValue[] values;
+
+        //constructor
+        public MyDictionary()
+        {
+            keys = new tKey[0];
+            values = new tValue[0];
+        }
+
+        public void Add(tKey key, tValue value)
+        {
+            tKey[] tempKey = keys;
+            tValue[] tempValue = values;
+
+            keys = new tKey[keys.Length + 1];
+            values = new tValue[values.Length + 1];
+
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+            }
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+        public tKey[] Keys
+        {
+            get { return keys; }
+        }
+
+        public tValue[] Values
+        {
+            get { return values; }
+        }
+
+    }
+
+}
+
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> books = new MyDictionary<int, string>();
+            books.Add(50, "Elon Musk ");
+            books.Add(30, "Google Sırları");
+            books.Add(200, "Clean Code");
+
+            Console.WriteLine("Kitap Fiyatı : " + books.Keys[0] + "\t" + "Kitabın Adı : " + books.Values[0]);
+            Console.WriteLine("Kitap Fiyatı : " + books.Keys[1] + "\t" + "Kitabın Adı : " + books.Values[1]);
+            Console.WriteLine("Kitap Fiyatı : " + books.Keys[2] + "\t" + "Kitabın Adı : " + books.Values[2]);
+        }
+
+
+    }
+}
